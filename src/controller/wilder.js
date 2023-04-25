50de0e57843adea3685cdc09f98d0a3405fe67ab
Charlotte Kieffer <kindred242@gmail.Com>
2023-04-25 15:04:18 +0200

    added the async/await

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -1,49 +1,43 @@
 const { dataSource } = require("../utils");
 const Wilder = require("../entity/Wilder");
+const { Long } = require("typeorm");
 
 module.exports ={
-    create: (req, res) => {
-        dataSource
+    create: async (req, res) => {
+        try {
+          await dataSource.getRepository(Wilder).save(req.body);
+          res.send("Created wilder");
+        } catch (error) {
+          console.log(error);
+          res.send("Error while creating wilder");
+        }},
+    find: async(req, res) => {
+        try{
+            await dataSource
+                .getRepository(Wilder)
+                .find(req.body);
+                res.send(data);
+        } catch (error) {
+            console.log(error);
+            res.send("Failed to find wilder");
+        }},
+    update: async(req, res) => {
+        try{
+        await dataSource
             .getRepository(Wilder)
-            .save(req.body)
-            .then(() => {
-                res.send("Created Wilder")
-            })
-            .catch(() => {
-                res.send("Error while creating wilder");
-            });
-    },
-    find: (req, res) => {
-        dataSource
+            .update({id :req.body.id}, {name :req.body.name});
+            res.send("Updated wilder");
+        } catch (error){
+            console.log(error);
+            res.send("Failed to update wilder")
+        }},
+    delete: async(req, res) => {
+        try {
+        await dataSource
             .getRepository(Wilder)
-            .find(req.body)
-            .then((data) => {
-                res.send(data)
-            })
-            .catch(() => {
-                res.send("Error while fetching wilder");
-            });
-    },
-    update: (req, res) => {
-        dataSource
-            .getRepository(Wilder)
-            .update({id :req.body.id}, {name :req.body.name})
-            .then(() => {
-                res.send("Updated wilder")
-            })
-            .catch(() => {
-                res.send("Error while updating wilder");
-            });
-    },
-    delete: (req, res) => {
-        dataSource
-            .getRepository(Wilder)
-            .delete({name :req.body.name})
-            .then(() => {
-                res.send("Deleted wilder")
-            })
-            .catch(() => {
-                res.send("Error while deleting wilder");
-            });
-    },
-};
+            .delete({name :req.body.name});
+            res.send("Deleted wilder")
+        } catch {
+            console.log(error);
+            res.send("Failed to delete wilder")
+        }}}
